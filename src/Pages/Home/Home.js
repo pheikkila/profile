957aadceca6dcf1a397f67a77864214a77e87271
pheikkila <presleyh@gmail.com>
2022-06-07 18:12:23 -0500

    fixing image extension

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,7 +1,5 @@
 // import logo from "../logo.svg";
-import { Container, Col, Row } from "react-bootstrap";
 import "./Home.css";
-import HomeSplash from "../../Images/HomeSplash.JPG";
 
 export default function Home() {
     return (
@@ -12,7 +10,7 @@ export default function Home() {
                     </ Container> */}
                 </div>
                 <p>
-                    Edit <code>src/App.js</code> and save to reload
+                    Edit <code>src/App.js</code> and save to reload.
                 </p>
                 <a
                     className="App-link"
